95e01897c82b402ac74abbecad009adf95939ed2
Tatyana Nemchina <23angorka07@gmail.com>
2025-12-10 18:02:02 +0300

    feat: added automated re-fetching

--- src/features/playlists/api/playlistsApi.ts
+++ src/features/playlists/api/playlistsApi.ts
@@ -1,43 +1,67 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import type { CreatePlaylistArgs, PlaylistData, PlaylistsResponse, UpdatePlaylistArgs } from "./playlistsApi.types";
+import type {
+  CreatePlaylistArgs,
+  PlaylistData,
+  PlaylistsResponse,
+  UpdatePlaylistArgs,
+} from "./playlistsApi.types";
 
 export const playlistsApi = createApi({
-    reducerPath: "playlistsApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl: import.meta.env.VITE_BASE_URL,
-        headers: {
-            'API-KEY': import.meta.env.VITE_API_KEY,
-        },
-        prepareHeaders: (headers) => {
-            headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
-            return headers
-        },
+  reducerPath: "playlistsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_BASE_URL,
+    headers: {
+      "API-KEY": import.meta.env.VITE_API_KEY,
+    },
+    prepareHeaders: (headers) => {
+      headers.set(
+        "Authorization",
+        `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`
+      );
+      return headers;
+    },
+  }),
+  tagTypes: ["Playlists"],
+  endpoints: (builder) => ({
+    getPlaylists: builder.query<PlaylistsResponse, void>({
+      query: () => "/playlists",
+      providesTags: ["Playlists"],
     }),
-    endpoints: (builder) => ({
-        getPlaylists: builder.query<PlaylistsResponse, void>({
-            query: () => '/playlists',
-        }),
-        createPlaylist: builder.mutation<{data: PlaylistData}, CreatePlaylistArgs>({
-            query: (body) => ({
-                method: 'POST',
-                url: '/playlists',
-                body
-            }),
-        }),
-        deletePlaylist: builder.mutation<void, string>({
-            query: (playlistId: string) => ({
-                method: 'DELETE',
-                url: `/playlists/${playlistId}`,
-            }),
-        }),
-        updatePlaylist: builder.mutation<void, {playlistId: string, body: UpdatePlaylistArgs}>({
-            query: ({playlistId, body}) => ({
-                method: 'PUT',
-                url: `/playlists/${playlistId}`,
-                body
-            }),
-        }),
-    })
+    createPlaylist: builder.mutation<
+      { data: PlaylistData },
+      CreatePlaylistArgs
+    >({
+      query: (body) => ({
+        method: "POST",
+        url: "/playlists",
+        body,
+      }),
+      invalidatesTags: ["Playlists"],
+    }),
+    deletePlaylist: builder.mutation<void, string>({
+      query: (playlistId: string) => ({
+        method: "DELETE",
+        url: `/playlists/${playlistId}`,
+      }),
+      invalidatesTags: ["Playlists"],
+    }),
+    updatePlaylist: builder.mutation<
+      void,
+      { playlistId: string; body: UpdatePlaylistArgs }
+    >({
+      query: ({ playlistId, body }) => ({
+        method: "PUT",
+        url: `/playlists/${playlistId}`,
+        body,
+      }),
+      invalidatesTags: ["Playlists"],
+    }),
+  }),
 });
 
-export const { useGetPlaylistsQuery, useCreatePlaylistMutation, useDeletePlaylistMutation, useUpdatePlaylistMutation } = playlistsApi
+export const {
+  useGetPlaylistsQuery,
+  useCreatePlaylistMutation,
+  useDeletePlaylistMutation,
+  useUpdatePlaylistMutation,
+} = playlistsApi;
