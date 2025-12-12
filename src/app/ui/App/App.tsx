3664f8cb5879a8e6b66a43802caf20dc0df5dda9
Tatyana Nemchina <23angorka07@gmail.com>
2025-12-12 17:44:17 +0300

    feat: added react-toastify

--- src/app/ui/App/App.tsx
+++ src/app/ui/App/App.tsx
@@ -1,17 +1,18 @@
-import { Header } from '@/common/components'
-import { Routing } from '@/common/routing'
-import s from './App.module.css'
+import { Header } from "@/common/components";
+import { Routing } from "@/common/routing";
+import s from "./App.module.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-
   return (
     <>
       <Header />
       <div className={s.layout}>
-      <Routing />
+        <Routing />
       </div>
+      <ToastContainer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/features/playlists/ui/PlaylistsPage/PlaylistItem/PlaylistCover/PlaylistCover.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistItem/PlaylistCover/PlaylistCover.tsx
@@ -6,6 +6,7 @@ import defaultCover from "@/assets/default-playlist-cover.png";
 import type { ChangeEvent } from "react";
 import type { Images } from "@/common/types";
 import s from "./PlaylistCover.module.css";
+import { toast } from "react-toastify";
 
 type Props = {
   playlistId: string;
@@ -25,12 +26,16 @@ export const PlaylistCover = ({ playlistId, images }: Props) => {
 
     if (!file) return;
     if (!allowedTypes.includes(file.type)) {
-      alert("Only JPEG, PNG or GIF images are allowed");
+      toast("Only JPEG, PNG or GIF images are allowed", {
+        type: "error",
+        theme: "colored",
+      });
       return;
     }
     if (file.size > maxSize) {
-      alert(
-        `The file is too large. Max size is ${Math.round(maxSize / 1024)} KB`
+      toast(
+        `The file is too large. Max size is ${Math.round(maxSize / 1024)} KB`,
+        { type: "error", theme: "colored" }
       );
       return;
     }
