8c15e864fd0f6e5108c88c21b78fe42c43141eea
Tatyana Nemchina <23angorka07@gmail.com>
2026-03-05 12:20:56 +0300

    feat: added infinite scrolling

--- src/features/tracks/ui/TracksPage.tsx
+++ src/features/tracks/ui/TracksPage.tsx
@@ -1,56 +1,58 @@
+import { useCallback, useEffect, useRef } from "react";
 import { useFetchTracksInfiniteQuery } from "../api/tracksApi";
-import s from "./TracksPage.module.css";
+import { TracksList } from "./TracksList/TracksList";
 
 export const TracksPage = () => {
-  const {
-    data,
-    isLoading,
-    isFetching,
-    isFetchingNextPage,
-    hasNextPage,
-    fetchNextPage,
-  } = useFetchTracksInfiniteQuery();
+  const { data, isFetching, isFetchingNextPage, hasNextPage, fetchNextPage } =
+    useFetchTracksInfiniteQuery();
 
   const pages = data?.pages.flatMap((page) => page.data) || [];
-  const loadMoreHandler = () => {
+  const observerRef = useRef<HTMLDivElement>(null);
+
+  const loadMoreHandler = useCallback(() => {
     if (hasNextPage && !isFetching) {
       fetchNextPage();
     }
-  };
+  }, [hasNextPage, isFetching, fetchNextPage]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries.length > 0 && entries[0].isIntersecting) {
+          loadMoreHandler();
+        }
+      },
+      {
+        root: null,
+        rootMargin: "100px",
+        threshold: 0.1,
+      },
+    );
+    const currentObserverRef = observerRef.current;
+    if (currentObserverRef) {
+      observer.observe(currentObserverRef);
+    }
+    return () => {
+      if (currentObserverRef) {
+        observer.unobserve(currentObserverRef);
+      }
+    };
+  }, [loadMoreHandler]);
 
   return (
     <div>
       <h1>Tracks page</h1>
-      <div className={s.list}>
-        {pages.map((track) => {
-          const { title, user, attachments } = track.attributes;
-
-          return (
-            <div key={track.id} className={s.item}>
-              <div>
-                <p>Title: {title}</p>
-                <p>Name: {user.name}</p>
-              </div>
-              {attachments.length ? (
-                <audio controls src={attachments[0].url} />
-              ) : (
-                "no file"
-              )}
-            </div>
-          );
-        })}
-        {!isLoading && (
-          <>
-            {hasNextPage ? (
-              <button onClick={loadMoreHandler} disabled={isFetching}>
-                {isFetchingNextPage ? "Loading..." : "Load More"}
-              </button>
-            ) : (
-              <p>Nothing more to load</p>
-            )}
-          </>
-        )}
-      </div>
+      <TracksList tracks={pages} />
+      {hasNextPage && (
+        <div ref={observerRef}>
+          {isFetchingNextPage ? (
+            <div>Loading more tracks...</div>
+          ) : (
+            <div style={{ height: "20px" }}></div>
+          )}
+        </div>
+      )}
+      {!hasNextPage && pages.length > 0 && <p>Nothing more to load</p>}
     </div>
   );
 };
